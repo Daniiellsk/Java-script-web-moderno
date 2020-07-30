0514bcef08939b0e56f70bbb5224f3571bd3aacb
Daniillsk <vitor.daniel87926@gmail.com>
2020-07-30 14:15:42 -0300

    Aulas de hoje
    aprendendo a usar "If" e  "else"

---
+++ Controle/IfElseIf.js
@@ -0,0 +1,19 @@
+Number.prototype.entre = function (inicio, fim){
+    return this >= inicio && this <= fim
+}
+
+const resultado = function (nota){
+    if (nota.entre(9, 10)) {
+        console.log('Quadro de Honra:',nota)
+    } else if (nota.entre(7, 8.99)) {
+        console.log('Aprovado:', nota)
+    } else if (nota.entre(4, 6.99)){
+        console.log('Recuperação:', nota)
+    } else if (nota.entre(0, 3.99)){
+        console.log('Reprovado:', nota)
+    } else {
+        console.log('Nota Invalida!')
+    }
+}
+
+resultado(10)
