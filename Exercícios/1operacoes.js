d4c3e2d78b5c8cba109d20b01c629c4392e67dc2
Vitor Daniel <65976033+DanieelAl@users.noreply.github.com>
2020-09-27 17:47:50 -0300

    exercícios 27 de Set

--- Exercícios/1operacoes.js
+++ Exercícios/1operacoes.js
@@ -1,3 +1,6 @@
+/*Proposta:
+01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,multiplicação e divisão desses valores.*/
+
 function operaçõesMatematicas1(a , b) {
     console.log(a + b, a - b, a * b, a / b)
 }
